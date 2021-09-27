c6e995ec2269e7b1c094420ce3aeabd1e448ac32
ksfastners1 <ksfastners619@gmail.com>
2021-09-28 01:14:23 +0530

    second

--- sample1/settings.py
+++ sample1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b=ylj@a*jy34d)xgo-o%t6t*mo6(6z2_0s=kcctfxe8gv4+x)g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
